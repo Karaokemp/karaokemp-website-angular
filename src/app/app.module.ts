ae736833dc0985c7dcbb1792fb0baad4a0850f75
admin אביגיל הרמתי <dreckguy@gmail.com>
2018-06-12 16:07:40 +0300

    working nice menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { HomeComponent } from './home/home.component';
 import { YoutubeComponent } from './youtube/youtube.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { AboutComponent } from './about/about.component';
+import { PerformancesComponent } from './performances/performances.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'youtube', component: YoutubeComponent },
+  { path: 'statistics', component: StatisticsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'performances', component: PerformancesComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', component: HomeComponent }
+
 ];
 
 @NgModule({
@@ -19,7 +27,10 @@ const appRoutes: Routes = [
     HomeComponent,
     YoutubeComponent,
     PageNotFoundComponent,
-    NavBarComponent
+    NavBarComponent,
+    StatisticsComponent,
+    AboutComponent,
+    PerformancesComponent
   ],
   imports: [
     BrowserModule,
