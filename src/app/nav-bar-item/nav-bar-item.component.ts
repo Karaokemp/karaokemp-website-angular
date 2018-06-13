9b0e20bb80fa0ada5249bd3ecfba7d42fc646840
admin אביגיל הרמתי <dreckguy@gmail.com>
2018-06-13 13:32:51 +0300

    try to export menu-item

--- src/app/nav-bar-item/nav-bar-item.component.ts
+++ src/app/nav-bar-item/nav-bar-item.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-bar-item',
   templateUrl: './nav-bar-item.component.html',
-  styleUrls: ['./nav-bar-item.component.css']
+  styleUrls: ['../nav-bar/nav-bar.component.css']
 })
 export class NavBarItemComponent implements OnInit {
 
