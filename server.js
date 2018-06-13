d9381f13bd72a9e4573e90ee564c7174d461d662
admin אביגיל הרמתי <dreckguy@gmail.com>
2018-06-13 09:27:41 +0300

    fix routing!

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
   app.use(express.static('dist/website-frontend'));
 
   app.get('/*', function(req, res) {
-    res.redirect(301, '/');
+    let path = `${__dirname}/dist/website-frontend/index.html`;
+    console.log(`path: ${path}`);
+    res.sendFile(path);
   });
 
   app.listen(PORT);
